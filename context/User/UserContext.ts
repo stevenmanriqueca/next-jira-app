b16e20a0b80f5795314593c2af9c15e3ec144289
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-14 12:00:45 -0500

    Added interface for ContextUser

--- context/User/UserContext.ts
+++ context/User/UserContext.ts
@@ -1,9 +1,11 @@
 import { createContext } from 'react';
 import { State, Login } from '../../interfaces/context-user';
+import { Register } from '../../interfaces/context-user/index';
 
 export type ContextProps = {
   state: State;
   loginUser: (data: Login) => void;
+  registerUser: (data: Register) => void;
 };
 
 export const UserContext = createContext<ContextProps>({} as ContextProps);
