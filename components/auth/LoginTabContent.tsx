da98e560c26bfb1afc018f4761c257d1ff054a31
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-14 12:04:22 -0500

    Added function of register user

--- components/auth/LoginTabContent.tsx
+++ components/auth/LoginTabContent.tsx
@@ -3,7 +3,6 @@ import { Stack, TextField, Button } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { PasswordAdornment } from "../ui";
 import { UserContext } from "../../context/User/UserContext";
-import { SnackbarToast } from "../ui/SnackbarToast";
 
 type FormData = {
   email: string;
@@ -14,58 +13,55 @@ export const LoginTabContent = (): JSX.Element => {
 
   const [showPassword, setShowPassword] = useState<boolean>(false);
 
-  const { state: { ui: { error } }, loginUser } = useContext(UserContext);
+  const { loginUser } = useContext(UserContext);
 
   const { register, formState: { errors }, handleSubmit } = useForm<FormData>();
 
   const onSubmit = ({ email, password }: FormData) => loginUser({ email, password });
 
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Stack spacing={2}>
-          <TextField autoFocus label="Email" variant="outlined" fullWidth
-            {...register("email", {
-              required: {
-                value: true,
-                message: "Email requiered",
-              },
-              pattern: {
-                value:
-                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                message: "The email is incorrect",
-              },
-            })}
-            helperText={String(errors.email?.message || "")}
-            error={Boolean(errors?.email)}
-            autoComplete="off"
-          />
-          <TextField label="Password" variant="outlined" autoComplete="off"
-            {...register("password", {
-              required: {
-                value: true,
-                message: "Password is required",
-              },
-              minLength: {
-                value: 5,
-                message: "The password must have at least 5 characters",
-              },
-            })}
-            helperText={String(errors.password?.message || "")}
-            error={Boolean(errors?.password)}
-            type={showPassword ? "text" : "password"}
-            InputProps={{
-              endAdornment: (
-                <PasswordAdornment showPassword={showPassword} setShowPassword={setShowPassword} />
-              ),
-            }}
-          />
-          <Button variant="contained" onClick={handleSubmit(onSubmit)} type="submit">
-            Login
-          </Button>
-        </Stack>
-      </form>
-      {error.message.length > 0 && <SnackbarToast open={true} message={error.message} status="error" />}
-    </>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={2}>
+        <TextField autoFocus label="Email" variant="outlined" fullWidth
+          {...register("email", {
+            required: {
+              value: true,
+              message: "Email requiered",
+            },
+            pattern: {
+              value:
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: "The email is incorrect",
+            },
+          })}
+          helperText={String(errors.email?.message || "")}
+          error={Boolean(errors?.email)}
+          autoComplete="off"
+        />
+        <TextField label="Password" variant="outlined" autoComplete="off"
+          {...register("password", {
+            required: {
+              value: true,
+              message: "Password is required",
+            },
+            minLength: {
+              value: 5,
+              message: "The password must have at least 5 characters",
+            },
+          })}
+          helperText={String(errors.password?.message || "")}
+          error={Boolean(errors?.password)}
+          type={showPassword ? "text" : "password"}
+          InputProps={{
+            endAdornment: (
+              <PasswordAdornment showPassword={showPassword} setShowPassword={setShowPassword} />
+            ),
+          }}
+        />
+        <Button variant="contained" onClick={handleSubmit(onSubmit)} type="submit">
+          Login
+        </Button>
+      </Stack>
+    </form>
   );
 };
