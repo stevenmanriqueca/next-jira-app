144769b502996e223f6b02028da72c49ad9279ef
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-15 07:10:52 -0500

    Added ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
 import { UserProvider } from '../context/User/UserProvider';
+import { theme } from '../styles/palette/theme';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <ThemeProvider theme={theme}>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </ThemeProvider>
   );
 }
 
