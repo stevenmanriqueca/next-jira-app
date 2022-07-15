0da35ca839485b896258857f05bfc9e88c16c998
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-15 07:09:39 -0500

    Created palette

---
+++ styles/palette/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            light: "",
+            main: "#f5f5f5",
+            dark: "#303f9f",
+        }
+    }
+})
